83e8936ce540bc293ad2c7e22df1a84c3d253d95
Erpheus <erpheus@gmail.com>
2013-03-16 10:50:25 +0100

    Initial commitº

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notesync::Application.config.secret_token = 'ed18f927879bef69f004a093dd785274f9e80f471fed8c8c2b2dfa56b0c67d5b44250ff5caae660d864320ac40c8da9c5dfe6fc3ddc10e29f105b6d65a3bfdfa'
