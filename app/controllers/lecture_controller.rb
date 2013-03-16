ca7f36082ff1858ba2dbc7ae1b5e3242880562a3
Erpheus <erpheus@gmail.com>
2013-03-16 11:38:22 +0100

    goodbye activemodel
    first couch rest model and initial controllers

--- app/controllers/lecture_controller.rb
+++ app/controllers/lecture_controller.rb
@@ -1,8 +1,14 @@
 class LectureController < ApplicationController
   def create
+    session[:note] = Note.new("aaa")
+    redirect_to lecture_edit_path()
   end
 
   def edit
+    @note = session[:note]
+    if !@note
+      redirect_to lecture_create_path()
+    end
   end
 
   def addParagraph
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,10 @@
 class Note < CouchRest::Model::Base
+
+	property :name, String, :default => ""
+	collection_of :paragraphs, :className => "Paragraph"
+
+	def initialize(name)
+		self.name = name
+	end
+
 end
